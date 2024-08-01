a2f495cd3349f19ce8b5e0d179748840088c6885
ankitsir4u <ankitsir4u@gmail.com>
2024-08-01 21:58:23 +0530

    new

---
+++ reverse_number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,rem,rev=0;
+    printf("enter a number:\n");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        rem=n%10;
+        rev=rev*10+rem;
+        n=n/10;
+
+    }
+    printf("the reverse number is :-%d",rev);
+    return 0;
+}
