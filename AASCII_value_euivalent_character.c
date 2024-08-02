1a5c1549e0ea87a41cac3d73021b682d4d6e478c
ankitsir4u <ankitsir4u@gmail.com>
2024-08-02 13:36:57 +0530

    for loop

--- AASCII_value_euivalent_character.c
+++ AASCII_value_euivalent_character.c
@@ -2,8 +2,11 @@
 int main()
 {
     int x=65;
-    printf("%d\n",x);
-    char ch=(char)x;
-    printf("%c\n",ch);
+    for(int i=65;i<=90;i++)
+    {
+        char ch =(char)i;
+        printf("%c->",ch);
+        printf("%d\n",i);
+    }
     return 0;
 }
