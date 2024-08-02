08914682f73ef6ba8ad475f4e02c7e545468bb8f
ankitsir4u <ankitsir4u@gmail.com>
2024-08-02 16:30:00 +0530

    number

--- square_pattern.c
+++ square_pattern.c
@@ -6,11 +6,11 @@ int main()
     scanf("%d",&r);
     printf("enter column:\n");
     scanf("%d",&c);
-    for(int i=1;i<=r;i++)
+    for(int i=1;i<=r;i++)// it is for row line
     {
-        for (int j=1;j<=c;j++)
+        for (int j=1;j<=c;j++) //this is responsible for printing
         {
-            printf("%d",j);
+            printf("%d",i);
         }
 
     
